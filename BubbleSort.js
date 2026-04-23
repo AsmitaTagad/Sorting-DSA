9749c06e0ae18c5c6d4a71eaef526c270991c9e0
Asmita Tagad <asmitatagad18@gmail.com>
2026-04-23 17:21:23 +0530

    Bubble Sort

---
+++ BubbleSort.js
@@ -0,0 +1,10 @@
+let arr=[5,10,8,13,7]
+
+for(let i=0; i< arr.length; i++){
+    for(let j=0; j<= arr.length-1-i; j++){
+        if(arr[j] > arr[j+1]){
+            [arr[j], arr[j+1]]= [arr[j+1], arr[j]]
+        }
+    }
+}
+console.log(arr)
