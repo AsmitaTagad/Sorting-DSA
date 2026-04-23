52daf14f1fcab6aeef0e0d4715b2abe6471a9c4e
Asmita Tagad <asmitatagad18@gmail.com>
2026-04-23 12:33:08 +0530

    Selection Using Brute force

---
+++ SelectionSort.js
@@ -0,0 +1,16 @@
+let arr=[5,2,9,1];
+
+function SelectionSort(arr){
+    for(let i=0; i< arr.length; i++){
+        let min=i;
+        for(let j=i+1; j< arr.length; j++){
+            if(arr[j] < arr[min]){
+                min=j;
+            }
+        }
+        [arr[i], arr[min]]= [arr[min], arr[i]]
+    }
+    return arr;
+}
+
+console.log(SelectionSort(arr))
